a6189ffec0228123f6d18c928df261f2779d9840
sakshi-sawant06 <sawantsakshi2021@gmail.com>
2026-09-20 14:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1412,9 +1412,7 @@ def logout():
 # RUN APPLICATION
 # ============================================================
 
- init_db()
+init_db()
 
 if __name__ == "__main__":
-
-
-    app.run(debug=True )
+   app.run(debug=True )
